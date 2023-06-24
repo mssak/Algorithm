71b1960e73510a03b7fcc2eff6ee46e38763c2b6
mssak <54972855+mssak@users.noreply.github.com>
2023-06-24 23:03:43 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cin>>a;
+    while(a--){
+        string s;
+        cin>>s;
+        cout<<s[0]<<s[s.size()-1]<<endl;
+    }
+}
