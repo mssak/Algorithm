54ed40939b7bc9ebb95d8762365ae707914ab9f0
mssak <54972855+mssak@users.noreply.github.com>
2023-07-16 00:35:26 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 88 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.cc
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    priority_queue<int> arr[6];
+
+    int n,p;
+    cin>>n>>p;
+
+    int ans=0;
+    while(n--){
+        int a,b;
+        cin>>a>>b;
+        a--;
+
+        while(!arr[a].empty() and arr[a].top()>b){
+            arr[a].pop();
+            ans++;
+        }
+        if(arr[a].empty() or arr[a].top()!=b){
+            arr[a].push(b);
+            ans++;
+        }
+    }
+    cout<<ans;
+}
