9e4b1b850f4942e49295c2b0e2c13c4a446014b1
mssak <54972855+mssak@users.noreply.github.com>
2023-07-21 08:13:43 +0900

    [Silver I] Title: 정수 삼각형, Time: 8 ms, Memory: 2876 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -2,43 +2,46 @@
 #define MAX 500
 using namespace std;
 
+int f(int y,int x) 
+{
+    return (y-1)*y/2+x-1; //y번째줄 x번째 원소의 인덱스
+}
+
 int main()
 {
     cin.tie(NULL);
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    int arr[MAX][MAX];
-    int dp[MAX][MAX] = {0};
+    int arr[MAX*(MAX+1)/2];
+    int dp[MAX*(MAX+1)/2]={0};
 
+    //입력
     int n;
-    cin >> n;
+    cin>>n;
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j <= i; j++) {
-            cin >> arr[i][j];
+    int m=n*(n+1)/2;
+    for(int i=0;i<m;i++){
+        cin>>arr[i];
+    }
+
+    //계산
+    dp[0]=arr[0];
+
+    for(int i=2;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            if(j==1){
+                dp[f(i,j)]=dp[f(i-1,j)]+arr[f(i,j)];
+            }
+            else if(j==i){
+                dp[f(i,j)]=dp[f(i-1,j-1)]+arr[f(i,j)];
+            }
+            else{
+                dp[f(i,j)]=max(dp[f(i-1,j)],dp[f(i-1,j-1)])+arr[f(i,j)];
+            }
         }
     }
 
-    dp[0][0] = arr[0][0];
-
-    for(int i = 1; i < n; i++) {
-        for(int j = 0; j <= i; j++) {
-            if(j == 0) {
-                dp[i][j] = dp[i-1][j] + arr[i][j];
-            }
-            else if(j == i) {
-                dp[i][j] = dp[i-1][j-1] + arr[i][j];
-            }
-            else {
-                dp[i][j] = max(dp[i-1][j], dp[i-1][j-1]) + arr[i][j];
-            }
-        }
-    }
-
-    int ans = 0;
-    for(int i = 0; i < n; i++) {
-        ans = max(ans, dp[n-1][i]);
-    }
-    cout << ans;
+    //출력
+    cout<<*max_element(dp+f(n,1),dp+f(n,n)+1);
 }
