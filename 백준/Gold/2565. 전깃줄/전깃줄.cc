ca89305a9c39c8c38e191e8a5e09b85de406dcb7
mssak <54972855+mssak@users.noreply.github.com>
2023-07-24 17:00:56 +0900

    [Gold V] Title: 전깃줄, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/2565. 전깃줄/전깃줄.cc
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int n;
+    cin>>n;
+
+    vector<pair<int,int>> arr;
+    int dp[501];
+
+    for(int i=0;i<n;i++){
+        int a,b;
+        cin>>a>>b;
+        arr.push_back({a,b});
+    }
+
+    for(int i=0;i<501;i++){
+        dp[i]=1;
+    }
+
+    sort(arr.begin(),arr.end());
+
+    for(int i=1;i<n;i++){
+        for(int j=0;j<i;j++){
+            if(arr[i].second>arr[j].second && dp[i]<dp[j]+1){
+                dp[i]=dp[j]+1;
+            }
+        }
+    }
+
+    cout<<n-*max_element(dp,dp+n);
+}
