d603609656a4a8683a7bb5afb300936087a2b6b3
mssak <54972855+mssak@users.noreply.github.com>
2023-07-27 03:12:51 +0900

    [Gold V] Title: 평범한 배낭, Time: 12 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,41 +1,24 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+
 using namespace std;
 
-pair<int,int> arr[100];
-int dp[100][100'001]={0};
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    
+    int n{}, k{};
+    int dp[100'001]{};
+    int v{}, c{};
 
-int f(int a,int w)
-{
-    if(w<0){
-        return INT_MIN;
-    }
-    else if(a<0 or w==0){
-        return 0;
-    }
-    else if(dp[a][w]){
-        return dp[a][w];
-    }
-    else{
-        dp[a][w]=max(f(a-1,w),f(a-1,w-arr[a].first)+arr[a].second);
-        return dp[a][w];
-    }
-}
+    cin >> n >> k;
+    while (n--) {
+        cin >> v >> c;
 
-int main()
-{
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
-
-    int n,k;
-    cin>>n>>k;
-
-    for(int i=0;i<n;i++){
-        int a,b;
-        cin>>a>>b;
-        arr[i]={a,b};
+        for (int i{k}; i >= v; i--) dp[i] = max(dp[i], dp[i - v] + c);
     }
 
-    f(n-1,k);
-    cout<<dp[n-1][k];
+    cout << dp[k];
+
+    return 0;
 }
