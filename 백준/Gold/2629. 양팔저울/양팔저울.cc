c19008f2385f025f1221af138f6d1dded3801b58
mssak <54972855+mssak@users.noreply.github.com>
2023-07-29 04:31:46 +0900

    [Gold III] Title: 양팔저울, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

---
+++ 백준/Gold/2629. 양팔저울/양팔저울.cc
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 40'001
+bool dp[2][MAX]={false};
+
+int main()
+{
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int n;
+    cin>>n;
+
+    int flag=0;
+
+    for(int i=0;i<n;i++){
+        int a;
+        cin>>a;
+        dp[flag][0]=true;
+        dp[flag^1][a]=true;
+        for(int j=0;j<MAX;j++){
+            if(dp[flag][j]){
+                dp[flag^1][j]=true;
+                dp[flag^1][abs(j-a)]=true;
+                if(j+a<MAX){
+                    dp[flag^1][j+a]=true;
+                }
+            }
+        }
+        flag^=1;
+    }
+
+    int m;
+    cin>>m;
+    while(m--){
+        int a;
+        cin>>a;
+        if(dp[flag][a]){
+            cout<<"Y\n";
+        }
+        else{
+            cout<<"N\n";
+        }
+    }
+}
