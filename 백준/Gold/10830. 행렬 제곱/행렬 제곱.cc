8cfc415ce06deefcb13a6cfa43e3c109114047a6
mssak <54972855+mssak@users.noreply.github.com>
2023-08-11 16:16:10 +0900

    [Gold IV] Title: 행렬 제곱, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/10830. 행렬 제곱/행렬 제곱.cc
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.cc
@@ -1,68 +1,68 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define MAX 
 using namespace std;
 typedef long long ll;
 typedef vector<vector<ll>> vv;
 typedef vector<ll> v;
 
-ll n,B;
-vv arr(5,v(5));
+ll n, B;
+vv arr(5, v(5));
+vv result(5, v(5, 0));
+vv temp(5, v(5, 0));
 
-vv mul(vv a,vv b)
-{
-    vv r(5,v(5,0));
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            for(int k=0;k<n;k++){
-                r[i][j]+=a[i][k]*b[k][j];
+void mul(vv& a, vv& b) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            temp[i][j] = 0;
+            for (int k = 0; k < n; k++) {
+                temp[i][j] += a[i][k] * b[k][j];
             }
-            r[i][j]%=1'000;
+            temp[i][j] %= 1'000;
         }
     }
-    return r;
+    swap(a, temp);
 }
 
-vv f(vv a,ll b)
-{
-    if(b==1){
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                a[i][j]%=1000;
+void f(ll b) {
+    if (b == 1) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] %= 1000;
             }
         }
-        return a;
+        return;
     }
-    vv half=f(a,b/2);
-    vv r=mul(half,half);
-    if(b%2==1){
-        r=mul(r,a);
+
+    f(b / 2);
+    mul(arr, arr);
+    if (b % 2 == 1) {
+        mul(arr, result);
     }
-    return r;
 }
 
-void print(vv a)
-{
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            printf("%d ",a[i][j]);
+void print(vv& a) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", a[i][j]);
         }
         printf("\n");
     }
 }
 
-int main()
-{
+int main() {
     cin.tie(NULL);
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    cin>>n>>B;
+    cin >> n >> B;
 
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cin>>arr[i][j];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+            result[i][j] = arr[i][j];
         }
     }
 
-    print(f(arr,B));
+    f(B);
+    print(arr);
 }
