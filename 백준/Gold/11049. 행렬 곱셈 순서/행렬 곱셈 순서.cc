13db3dc7c97cd4b1d64c029688c5e5a07e97e29c
mssak <54972855+mssak@users.noreply.github.com>
2023-08-14 03:21:22 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 40 ms, Memory: 3000 KB -BaekjoonHub

---
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.cc
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define MAX 500 
+using namespace std;
+
+int dp[MAX][MAX];
+int mat[MAX][2];
+
+int main()
+{
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int n;
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        int a,b;
+        cin>>a>>b;
+        mat[i][0]=a;
+        mat[i][1]=b;
+    }
+
+    for(int i=1;i<n;i++){
+        for(int j=0;i+j<n;j++){
+            dp[j][i+j]=INT_MAX;
+            for(int k=j;k<=i+j;k++){
+                dp[j][j+i]=min(dp[j][j+i],dp[j][k]+dp[k+1][j+i]+mat[j][0]*mat[k][1]*mat[i+j][1]);
+            }
+        }
+    }
+
+
+    cout<<dp[0][n-1];
+
+}
