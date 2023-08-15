ddf7fa1bd09bfaed474d45de424fff8fb629f7ac
mssak <54972855+mssak@users.noreply.github.com>
2023-08-16 02:56:33 +0900

    [Gold III] Title: 파일 합치기, Time: 264 ms, Memory: 3000 KB -BaekjoonHub

---
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.cc
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+#define MAX 500
+using namespace std;
+
+int arr[MAX];
+int dp[MAX][MAX];
+
+int f(int a,int b)
+{
+    if(dp[a][b]){
+        return dp[a][b];
+    }
+    else if(a==b){
+        return 0;
+    }
+    else{
+        dp[a][b]=INT_MAX;
+        for(int i=a;i<b;i++){
+            dp[a][b]=min(dp[a][b],f(a,i)+f(i+1,b));
+        }
+        dp[a][b]+=accumulate(arr+a,arr+b+1,0);
+        return dp[a][b];
+    }
+}
+
+int main()
+{
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int t;
+    cin>>t;
+
+    while(t--){
+        int n;
+        cin>>n;
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j]=0;
+            }
+        }
+
+        cout<<f(0,n-1)<<"\n";
+    }
+}
