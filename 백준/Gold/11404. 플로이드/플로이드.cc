365b20c1ccf12cd5b64718ea70b09f3a9d6e196e
mssak <54972855+mssak@users.noreply.github.com>
2023-08-25 05:33:10 +0900

    [Gold IV] Title: 플로이드, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#define MAX 101
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int arr[MAX][MAX];
+
+    int n,m;
+    cin>>n>>m;
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            arr[i][j]=1e9;
+        }
+    }
+
+    for(int i=0;i<m;i++){
+        int a,b,c;
+        cin>>a>>b>>c;
+        arr[a][b]=min(c,arr[a][b]);
+    }
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            for(int k=1;k<=n;k++){
+                if(j==k || j==i || k==i){
+                    continue;
+                }
+                arr[j][k]=min(arr[j][k],arr[j][i]+arr[i][k]);
+            }
+        }
+    }
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cout<<(arr[i][j]==1e9?0:arr[i][j])<<" ";
+        }
+        cout<<"\n";
+    }
+}
