0a538e58882c8036200b36b44abab0f17b19197d
mssak <54972855+mssak@users.noreply.github.com>
2023-08-28 19:58:15 +0900

    [Silver I] Title: 1로 만들기 2, Time: 0 ms, Memory: 3012 KB -BaekjoonHub

---
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#define MAX 
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int n;
+    cin>>n;
+
+    bool dp[1'000'001]={0};
+
+    queue<vector<int>> q;
+    q.push({n});
+
+    while(true){
+        auto cv=q.front();
+        q.pop();
+        int c=cv.back();
+
+        if(c==1){
+            cout<<cv.size()-1<<"\n";
+            for(auto i:cv){
+                cout<<i<<" ";
+            }
+            return 0;
+        }
+        else if(dp[c]){
+            continue;
+        }
+
+        dp[c]=true;
+
+        if(c%3==0 && !dp[c/3]){
+            vector<int> v(cv);
+            v.push_back(c/3);
+            q.push(v);
+        }
+        if(c%2==0 && !dp[c/2]){
+            vector<int> v(cv);
+            v.push_back(c/2);
+            q.push(v);
+        }
+        if(c-1>0 && !dp[c-1]){
+            vector<int> v(cv);
+            v.push_back(c-1);
+            q.push(v);
+        }
+    }
+}
