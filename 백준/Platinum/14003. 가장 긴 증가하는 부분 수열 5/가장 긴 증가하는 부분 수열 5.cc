027fab345dc6a963dd98875343e3afb30b5088c4
mssak <54972855+mssak@users.noreply.github.com>
2023-08-30 16:04:50 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 216 ms, Memory: 26352 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -9,47 +9,42 @@ int main() {
     cin.tie(0);
     cout.tie(0);
 
-    int N;
-    cin >> N;
+    int n;
+    cin >> n;
 
-    vector<int> arr(N);
-    for(int i = 0; i < N; i++) {
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
     vector<int> LIS; // 실제 값들을 저장하는 LIS
     vector<int> idx; // LIS의 원소들의 원래 인덱스를 저장
-    vector<int> prev(N, -1); // 각 원소의 이전 원소의 인덱스를 저장
+    vector<int> prev(n, -1); // 각 원소의 이전 원소의 인덱스를 저장
 
-    for(int i = 0; i < N; i++) {
-        if(LIS.empty() || LIS.back() < arr[i]) {
-            if(!LIS.empty()) prev[i] = idx.back();
+    for(int i=0;i<n;i++){
+        if(LIS.empty() || LIS.back()<arr[i]){
             LIS.push_back(arr[i]);
             idx.push_back(i);
-        } else {
-            auto it = lower_bound(LIS.begin(), LIS.end(), arr[i]);
-            int pos = distance(LIS.begin(), it);
-            *it = arr[i];
-            idx[pos] = i;
-            if(pos > 0) prev[i] = idx[pos - 1];
+            if(i>0) prev[i]=idx[LIS.size()-2];
+        }
+        else{
+            auto it=lower_bound(LIS.begin(),LIS.end(),arr[i]);
+            *it=arr[i];
+            int pos=it-LIS.begin();
+            idx[pos]=i;
+            if(pos>0) prev[i]=idx[pos-1];
         }
     }
 
-    cout << LIS.size() << "\n";
-
-    // 실제 LIS 추적
-    int p = idx.back();
-    vector<int> actual_LIS;
-    while(p != -1) {
-        actual_LIS.push_back(arr[p]);
-        p = prev[p];
+    vector<int> ans;
+    int i=idx.back();
+    for(;i!=-1;i=prev[i]){
+        ans.push_back(arr[i]);
     }
 
-    reverse(actual_LIS.begin(), actual_LIS.end());
-    for(int val : actual_LIS) {
-        cout << val << " ";
+    cout<<LIS.size()<<"\n";
+    for(int i=ans.size()-1;i>=0;i--){
+        cout<<ans[i]<<" ";
     }
-    cout << "\n";
-
     return 0;
 }
