344c2e0016b8d0d504b55f767717d063f34e973c
mssak <54972855+mssak@users.noreply.github.com>
2023-09-04 02:36:14 +0900

    [Gold V] Title: 체스판 다시 칠하기 2, Time: 100 ms, Memory: 17540 KB -BaekjoonHub

--- 백준/Gold/25682. 체스판 다시 칠하기 2/체스판 다시 칠하기 2.cc
+++ 백준/Gold/25682. 체스판 다시 칠하기 2/체스판 다시 칠하기 2.cc
@@ -7,7 +7,6 @@ int main()
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    int w[MAX][MAX]{0};
     int b[MAX][MAX]{0};
     int n,m,k;
     cin>>n>>m>>k;
@@ -19,18 +18,13 @@ int main()
             char a;
             cin>>a;
             b[i][j] = b[i-1][j] + b[i][j-1] - b[i-1][j-1];
-            w[i][j] = w[i-1][j] + w[i][j-1] - w[i-1][j-1];
 
             if(a=='B'){
                 if(flag){
                     b[i][j]++;
-                }else{
-                    w[i][j]++;
                 }
             }else{
-                if(flag){
-                    w[i][j]++;
-                }else {
+                if(!flag){
                     b[i][j]++;
                 }
             }
@@ -44,8 +38,8 @@ int main()
     int ans=1e9;
     for(int i=0;i<=n-k;i++){
         for(int j=0;j<=m-k;j++){
-            ans=min(ans,w[i+k][j+k]-w[i+k][j]-w[i][j+k]+w[i][j]);
             ans=min(ans,b[i+k][j+k]-b[i+k][j]-b[i][j+k]+b[i][j]);
+            ans=min(ans,k*k-(b[i+k][j+k]-b[i+k][j]-b[i][j+k]+b[i][j]));
         }
     }
 
