bec40d018845a3c3381c1b12aa452dc3610650cb
mssak <54972855+mssak@users.noreply.github.com>
2023-09-09 20:26:57 +0900

    [Silver I] Title: 트리 순회, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.cc
+++ 백준/Silver/1991. 트리 순회/트리 순회.cc
@@ -1,94 +1,47 @@
-#include<bits/stdc++.h>
-#define MAX 27
+#include <iostream>
 using namespace std;
 
-char arr[MAX][2]{0};
 int n;
+char lc[30];
+char rc[30];
 
-void pre()
-{
-    bool v[MAX]{0};
-    stack<char> s;
-    v[1]=true;
-    v[0]=true;
-    s.push(1);
-
-    while(!s.empty()){
-        char c=s.top();
-        s.pop();
-        cout<<(char)(c+'A'-1);
-
-        for(int it=1;it>=0;it--){
-            char i=arr[c][it];
-            if(!v[i]){
-                v[i]=true;
-                s.push(i);
-            }
-        }
-    }
+void preorder(char root) {
+	if (root != '.') {
+		cout << root;
+		preorder(lc[root - 'A']);
+		preorder(rc[root - 'A']);
+	}
 }
 
-void inU(int a,bool v[])
-{
-    v[a]=true;
-    if(!v[arr[a][0]]){
-        inU(arr[a][0],v);
-    }
-    cout<<(char)(a+'A'-1);
-    if(!v[arr[a][1]]){
-        inU(arr[a][1],v);
-    }
+void inorder(char root) {
+	if (root != '.') {
+		inorder(lc[root - 'A']);
+		cout << root;
+		inorder(rc[root - 'A']);
+	}
 }
 
-void in()
-{
-    bool v[MAX]{0};
-    v[0]=true;
-    inU(1,v);
+void postorder(char root) {
+	if (root != '.') {
+		postorder(lc[root - 'A']);
+		postorder(rc[root - 'A']);
+		cout << root;
+	}
 }
 
-void postU(int a,bool v[])
+int main(void)
 {
-    v[a]=true;
-    for(int i:arr[a]){
-        if(!v[i]){
-            postU(i,v);
-        }
-    }
-    cout<<(char)(a+'A'-1);
-}
-
-
-void post()
-{
-    bool v[MAX]{0};
-    v[0]=true;
-    postU(1,v);
-}
-
-int main()
-{
-    cin.tie(NULL);
-    ios_base::sync_with_stdio(false);
-
-    cin>>n;
-
-    for(int i=0;i<n;i++){
-        char a,b,c;
-        cin>>a>>b>>c;
-        a=a-'A'+1;
-
-        if(b!='.'){
-            arr[a][0]=b-'A'+1;
-        }
-        if(c!='.'){
-            arr[a][1]=c-'A'+1;
-        }
-    }
-
-    pre();
-    cout<<"\n";
-    in();
-    cout<<"\n";
-    post();
+	cin >> n;
+	char p, l, r;
+	for (int i = 0; i < n; i++) {
+		cin >> p >> l >> r;
+		lc[p - 'A'] = l;
+		rc[p - 'A'] = r;
+	}
+	preorder('A');
+	cout << '\n';
+	inorder('A');
+	cout << '\n';
+	postorder('A');
+	return 0;
 }
