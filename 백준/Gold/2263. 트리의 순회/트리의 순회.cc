f1d3593dccde408e259a054976f08e1e2fc40b98
mssak <54972855+mssak@users.noreply.github.com>
2023-09-11 01:37:54 +0900

    [Gold I] Title: 트리의 순회, Time: 112 ms, Memory: 11788 KB -BaekjoonHub

--- 백준/Gold/2263. 트리의 순회/트리의 순회.cc
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.cc
@@ -1,36 +1,48 @@
-#include<bits/stdc++.h>
-#define MAX 100'000
-using namespace std;
-int n;
-int in[MAX];
-int post[MAX];
+#include <iostream>
+#include <vector>
+#include <unordered_map>
 
-void f(int s,int e,int p)
-{
-    if(s>e){
+using namespace std;
+
+vector<int> inorder, postorder;
+unordered_map<int, int> inorder_idx;
+
+void find_preorder(int in_start, int in_end, int post_start, int post_end) {
+    if (in_start > in_end || post_start > post_end) {
         return;
     }
 
-    int ri=find(in+s,in+e,post[p])-in;
-    cout<<in[ri]<<" ";
+    // 포스트오더의 마지막 원소는 현재 서브트리의 루트
+    int root = postorder[post_end];
+    cout << root << " ";
 
-    f(s,ri-1,p-(e-ri+1));
-    f(ri+1,e,p-1);
+    int root_idx_inorder = inorder_idx[root];  // 인오더에서의 루트 위치
+    int left_size = root_idx_inorder - in_start;  // 왼쪽 서브트리의 크기
+
+    // 왼쪽 서브트리 탐색
+    find_preorder(in_start, root_idx_inorder-1, post_start, post_start+left_size-1);
+    // 오른쪽 서브트리 탐색
+    find_preorder(root_idx_inorder+1, in_end, post_start+left_size, post_end-1);
 }
 
-int main()
-{
-    cin.tie(NULL);
-    ios_base::sync_with_stdio(false);
+int main() {
+    int n;
+    cin >> n;
 
-    cin>>n;
+    inorder.resize(n);
+    postorder.resize(n);
 
-    for(int i=0;i<n;i++){
-        cin>>in[i];
+    for (int i = 0; i < n; i++) {
+        cin >> inorder[i];
     }
-    for(int i=0;i<n;i++){
-        cin>>post[i];
+    for (int i = 0; i < n; i++) {
+        cin >> postorder[i];
     }
 
-    f(0,n-1,n-1);
+    for (int i = 0; i < n; i++) {
+        inorder_idx[inorder[i]] = i;
+    }
+
+    find_preorder(0, n-1, 0, n-1);
+    return 0;
 }
