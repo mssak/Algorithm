b9b5eef34af35d0b154321667928ddf53ea9054c
mssak <54972855+mssak@users.noreply.github.com>
2023-09-13 03:18:43 +0900

    [Gold V] Title: 집합의 표현, Time: 36 ms, Memory: 9840 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.cc
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.cc
@@ -1,51 +1,63 @@
-#include<bits/stdc++.h>
-#define MAX 1'000'000
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int arr[MAX];
+class UnionFind {
+private:
+    vector<int> parent; // 각 노드의 부모 노드 정보를 저장하는 배열
+    vector<int> rank;   // 트리의 높이를 저장하는 배열
 
-pair<int,int> f(int a,int b)
-{
-    if(a==arr[a]){
-        return {a,b};
+public:
+    UnionFind(int n) {
+        parent.resize(n + 1);
+        rank.resize(n + 1, 0);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
     }
-    return f(arr[a],b+1);
-}
 
-int main()
-{
-    cin.tie(NULL);
+    int find(int u) {
+        if (u == parent[u]) {
+            return u;
+        }
+        return parent[u] = find(parent[u]);
+    }
+
+    void merge(int u, int v) {
+        u = find(u);
+        v = find(v);
+
+        if (u == v) return; // 이미 같은 집합
+
+        // 랭크가 높은 트리에 낮은 트리를 합침
+        if (rank[u] > rank[v]) swap(u, v);
+
+        parent[u] = v;
+        if (rank[u] == rank[v]) rank[v]++;
+    }
+};
+
+int main() {
     ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    int n,m;
-    cin>>n>>m;
+    int N, M, c, a, b;
+    cin >> N >> M;
 
-    for(int i=0;i<=n;i++){
-        arr[i]=i;
-    }
+    UnionFind uf(N);
 
-    for(int i=0;i<m;i++){
-        int a,b,c;
-        cin>>a>>b>>c;
-
-        if(a==0){
-            auto [br,bt]=f(b,0);
-            auto [cr,ct]=f(c,0);
-
-            if(bt<ct){
-                arr[br]=cr;
-            }
-            else{
-                arr[cr]=br;
-            }
-        }
-        else{
-            if(f(b,0).first==f(c,0).first){
-                cout<<"yes\n";
-            }
-            else{
-                cout<<"no\n";
+    for (int i = 0; i < M; i++) {
+        cin >> c >> a >> b;
+        if (c == 0) {
+            uf.merge(a, b);
+        } else {
+            if (uf.find(a) == uf.find(b)) {
+                cout << "YES\n";
+            } else {
+                cout << "NO\n";
             }
         }
     }
+
+    return 0;
 }
