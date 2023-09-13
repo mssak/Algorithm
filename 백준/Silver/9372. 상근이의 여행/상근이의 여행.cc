ef6bdea97119faef2924caf9819b0327e0df6f3c
mssak <54972855+mssak@users.noreply.github.com>
2023-09-14 06:52:47 +0900

    [Silver IV] Title: 상근이의 여행, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define MAX 1'001
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int T;
+    cin>>T;
+
+    while(T--){
+        int n,m;
+        cin>>n>>m;
+        for(int i=0;i<m;i++){
+            int a,b;
+            cin>>a>>b;
+        }
+        cout<<n-1<<"\n";
+    }
+}
