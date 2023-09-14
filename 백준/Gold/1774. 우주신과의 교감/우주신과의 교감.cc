0af227da617f746b1fa791135b50422e8d563ac0
mssak <54972855+mssak@users.noreply.github.com>
2023-09-14 20:27:18 +0900

    [Gold III] Title: 우주신과의 교감, Time: 84 ms, Memory: 14640 KB -BaekjoonHub

--- 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.cc
+++ 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.cc
@@ -1,67 +1,62 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <cmath>
-#include <queue>
-#include <iomanip>
-
+#include<bits/stdc++.h>
+#define MAX 1000
 using namespace std;
+using dii=tuple<double,int,int>;
 
-typedef pair<int, int> pii;
-typedef pair<double, int> pdi; // 우선순위 큐를 위한 타입 (거리, 인덱스)
+int uf[MAX];
+int pos[MAX][2];
+vector<dii> edge;
 
-double getDistance(const pii &a, const pii &b) {
-    return sqrt(pow(a.first - b.first, 2) + pow(a.second - b.second, 2));
+double getd(int i,int j) {
+    return pow(pow(pos[i][0]-pos[j][0],2)+pow(pos[i][1]-pos[j][1],2),0.5);
 }
 
-double prim(int N, const vector<pii>& coords, const vector<vector<int>>& connected) {
-    vector<bool> visited(N, false);
-    priority_queue<pdi, vector<pdi>, greater<pdi>> pq; // 거리가 작은 것부터 pop 되도록 설정
-    pq.push({0.0, 0}); // 시작점
-    double answer = 0.0;
-
-    while (!pq.empty()) {
-        double dist = pq.top().first;
-        int cur = pq.top().second;
-        pq.pop();
-
-        if (visited[cur]) continue;
-        visited[cur] = true;
-        answer += dist;
-
-        for (int i = 0; i < N; i++) {
-            if (!visited[i]) {
-                if (find(connected[cur].begin(), connected[cur].end(), i) != connected[cur].end()) {
-                    pq.push({0.0, i}); // 이미 연결된 통로
-                } else {
-                    pq.push({getDistance(coords[cur], coords[i]), i});
-                }
-            }
-        }
+int f(int a) {
+    if(uf[a] != a) {
+        uf[a] = f(uf[a]);
     }
-
-    return answer;
+    return uf[a];
 }
 
 int main() {
-    int N, M;
-    cin >> N >> M;
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
 
-    vector<pii> coords(N);
-    for (int i = 0; i < N; i++) {
-        cin >> coords[i].first >> coords[i].second;
+    int n,m;
+    cin>>n>>m;
+
+    for(int i=0;i<n;i++){
+        uf[i]=i;
+        cin>>pos[i][0]>>pos[i][1];
     }
 
-    vector<vector<int>> connected(N);
-    for (int i = 0; i < M; i++) {
-        int a, b;
-        cin >> a >> b;
-        a--; b--;
-        connected[a].push_back(b);
-        connected[b].push_back(a);
+    for(int i=0;i<m;i++){
+        int a,b;
+        cin>>a>>b;
+        a--;b--;
+        int ar=f(a);
+        int br=f(b);
+
+        uf[ar]=br;
     }
 
-    cout << fixed << setprecision(2) << prim(N, coords, connected) << "\n";
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            edge.emplace_back(getd(i,j),i,j);
+        }
+    }
 
-    return 0;
+    sort(edge.begin(),edge.end());
+
+    double ans{0};
+    for(auto &[d,a,b] : edge){
+        int ar=f(a);
+        int br=f(b);
+        if(ar!=br){
+            ans+=d;
+            uf[ar]=br;
+        }
+    }
+
+    printf("%.2f",ans);
 }
