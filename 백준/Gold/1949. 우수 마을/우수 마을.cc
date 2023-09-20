d962e9fe7dcaed4baa7cb816bcd55722fb4f4ad6
mssak <54972855+mssak@users.noreply.github.com>
2023-09-20 20:56:51 +0900

    [Gold II] Title: 우수 마을, Time: 4 ms, Memory: 3268 KB -BaekjoonHub

--- 백준/Gold/1949. 우수 마을/우수 마을.cc
+++ 백준/Gold/1949. 우수 마을/우수 마을.cc
@@ -1,79 +1,38 @@
-#include<bits/stdc++.h>
-#define MAX 10'001
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int node[MAX];
-vector<int> edge[MAX];
-int p[MAX];
-int dp[MAX][2]{0};
+const int INF = 1e9;
+const int MAX_N = 10005;
 
-void findParent(int a)
+int N, C[MAX_N];
+vector<int> adj[MAX_N];
+int dp[MAX_N][2];
+
+void f(int x, int par)
 {
-    for(int i:edge[a]){
-        if(i!=p[a]){
-            p[i]=a;
-            findParent(i);
-        }
+    dp[x][1] = C[x];
+    for(int nxt : adj[x]){
+        if(nxt == par) continue;
+        f(nxt, x);
+        dp[x][0] += max(dp[nxt][0], dp[nxt][1]);
+        dp[x][1] += dp[nxt][0];
     }
 }
-
-int f(int a, int b)
-{
-    if (a <= 0) {
-        return 0;
-    }
-    if (dp[a][b] != -1) {
-        return dp[a][b];
-    }
-
-    if (b == 0) {
-        // 현재 마을을 '우수 마을'로 선택하지 않은 경우
-        int res = 0;
-        for (int i : edge[a]) {
-            if (i == p[a]) {
-                continue;
-            }
-            // 자식 마을을 '우수 마을'로 선택하지 않거나 선택하는 경우 중에서 최대 값을 선택
-            res += max(f(i, 0), f(i, 1));
-        }
-        return dp[a][b] = res;
-    } else if (b == 1) {
-        // 현재 마을을 '우수 마을'로 선택한 경우
-        int res = node[a];
-        for (int i : edge[a]) {
-            if (i == p[a]) {
-                continue;
-            }
-            // 자식 마을은 '우수 마을'로 선택하지 않아야 함
-            res += f(i, 0);
-        }
-        return dp[a][b] = res;
-    }
-}
-
 
 int main()
 {
-    cin.tie(NULL);
-    ios_base::sync_with_stdio(false);
-
-    int n;
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        cin>>node[i];
-        dp[i][1]=-1;
-        dp[i][0]=-1;
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cin >> N;
+    for(int i=1; i<=N; i++) cin >> C[i];
+    for(int i=0; i<N-1; i++){
+        int A, B;
+        cin >> A >> B;
+        adj[A].push_back(B);
+        adj[B].push_back(A);
     }
-
-    for(int i=0;i<n-1;i++){
-        int a,b;
-        cin>>a>>b;
-        edge[a].push_back(b);
-        edge[b].push_back(a);
-    }
-
-    p[1]=-1;
-    findParent(1);
-
-    cout<<max(f(1,0),f(1,1));
+    f(1, 0);
+    printf("%d\n", max(dp[1][1], dp[1][0]));
+    return 0;
 }
