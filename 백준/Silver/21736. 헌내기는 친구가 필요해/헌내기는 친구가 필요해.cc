e32e504a0e0bd2d4f4254859688540f4743f0bc1
mssak <cxz017@naver.com>
2024-05-28 14:55:31 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 20 ms, Memory: 3128 KB -BaekjoonHub

---
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.cc
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+#define MAX 
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int n,m;
+    cin>>n>>m;
+
+    char arr[600][600];
+    bool v[600][600]{0};
+
+    int sx,sy;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr[i][j];
+            if(arr[i][j]=='I'){
+                sx=j;
+                sy=i;
+            }
+        }
+    }
+
+    int ans=0;
+    pair<int,int> dir[4]={{0,-1},{-1,0},{1,0},{0,1}};
+
+
+    queue<pair<int,int>> q;
+    q.push({sy,sx});
+
+    while(q.size()){
+        auto [cy,cx]=q.front();
+        q.pop();
+
+        if(v[cy][cx]){
+            continue;
+        }
+
+        v[cy][cx]=true;
+        if(arr[cy][cx]=='P'){
+            ans++;
+        }
+
+        for(int i=0;i<4;i++){
+            int ny=cy+dir[i].first;
+            int nx=cx+dir[i].second;
+
+            if(ny>=0 && ny<n && nx>=0 && nx<m && v[ny][nx]==false && arr[ny][nx]!='X'){
+                
+                q.push({ny,nx});
+            }
+        }
+    }
+
+    if(ans==0){
+        cout<<"TT";
+        return 0;
+    }
+    cout<<ans;
+}
