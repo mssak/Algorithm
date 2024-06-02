4981d69afa0514fc2b07c18e4458263d59a9515f
mssak <cxz017@naver.com>
2024-06-02 19:29:42 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.cc
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.cc
@@ -1,47 +1,50 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 #define MAX 1001
 using namespace std;
 
-int main() {
+int main()
+{
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    list<pair<int, int>> lt;
+    list<pair<int,int>> lt;
     int n;
-    cin >> n;
+    cin>>n;
 
-    for (int i = 1; i <= n; i++) {
+    for(int i=1;i<=n;i++){
         int a;
-        cin >> a;
-        lt.push_back({i, a});
+        cin>>a;
+        lt.push_back({i,a});
     }
 
     auto it = lt.begin();
-    while (!lt.empty()) {
-        auto [num, m] = *it;
-        cout << num << " ";
+    while(1){
+        auto [num,m] = *it;
+        cout<<num<<" ";
         it = lt.erase(it);
+        if(it==lt.end()){
+            it=lt.begin();
+        }
 
-        if (lt.empty()) {
+        if(lt.empty()){
             break;
         }
 
-        // Adjust the iterator position after erasing the current balloon
-        if (m > 0) {
-            for (int i = 0; i < m - 1; i++) {
-                if (it == lt.end()) it = lt.begin();
+        if(m>0){
+            for(int i=0;i<m-1;i++){
                 it++;
-                if (it == lt.end()) it = lt.begin();
+                if(it==lt.end()){
+                    it=lt.begin();
+                }
             }
-        } else {
-            for (int i = 0; i < -m; i++) {
-                if (it == lt.begin()) it = lt.end();
+        }
+        else{
+            for(int i=0;i<-m;i++){
+                if(it==lt.begin()){
+                    it=lt.end();
+                }
                 it--;
             }
         }
-
-        // Ensure the iterator is within the valid range
-        if (it == lt.end()) it = lt.begin();
     }
-    return 0;
 }
