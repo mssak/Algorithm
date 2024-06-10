bdfc9c680ebf23fa45a5e3a795d1091c4de0cba9
mssak <cxz017@naver.com>
2024-06-11 00:27:46 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 12 ms, Memory: 2252 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.cc
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.cc
@@ -10,12 +10,12 @@ int main()
     int n,m;
     cin>>n>>m;
 
-    int arr[301][301];
     int sum[301][301];
     for(int i=1;i<=n;i++){
         for(int j=1;j<=m;j++){
-            cin>>arr[i][j];
-            sum[i][j]=arr[i][j]+sum[i][j-1]+sum[i-1][j]-sum[i-1][j-1];
+            int a;
+            cin>>a;
+            sum[i][j]=a+sum[i][j-1]+sum[i-1][j]-sum[i-1][j-1];
         }
     }
 
