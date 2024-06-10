c58d8307d7b1e16322daf0337f3c9fd878274a43
mssak <cxz017@naver.com>
2024-06-10 14:57:09 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 3.72 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.cpp
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.cpp
@@ -1,33 +1,29 @@
 #include <string>
 #include <vector>
-#include <bits/stdc++.h>
-
+#include<iostream>
 using namespace std;
 
 int solution(string skill, vector<string> skill_trees) {
     int answer = 0;
-    int arr[26]{0};
-    for(int i=0;i<skill.size();i++){
-        arr[skill[i]-'A']=i+1;
-    }
-    
-    for(string i:skill_trees){
-        int pos=1;
-        bool can=true;
-        for(char c:i){
-            if(arr[c-'A']!=0){
-                if(pos==arr[c-'A']){
-                    pos++;
-                }
-                else{
-                    can=false;
-                    break;
+    for (int i = 0; i < skill_trees.size(); i++) {
+        vector<char> compare;
+
+        for (int k = 0; k < skill_trees[i].size(); k++) {
+            for (int j = 0; j < skill.size(); j++) {
+                if (skill[j] == skill_trees[i][k]) {
+                    char tmp = skill[j];
+                    compare.push_back(tmp);
                 }
             }
         }
-        if(can){
-            answer++;
+        bool check = true;;
+        for (int c = 0; c < compare.size(); c++) {
+            if (skill[c] != compare[c]) {
+                check = false;
+            }
         }
+        if (check)answer++;
     }
+
     return answer;
 }
