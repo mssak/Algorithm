ca3631aebf237a777d5071423b6ccfa7a75589a5
mssak <cxz017@naver.com>
2025-02-15 10:22:34 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define MAX 
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int n,m;
+    cin>>n>>m;
+
+    cout<<n*m-1;
+}
