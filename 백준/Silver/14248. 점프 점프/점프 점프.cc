8146b6af0f713f0601e8273aa941afae363c6f5a
mssak <cxz017@naver.com>
2025-06-27 00:23:26 +0900

    [Silver II] Title: 점프 점프, Time: 8 ms, Memory: 2388 KB -BaekjoonHub

---
+++ 백준/Silver/14248. 점프 점프/점프 점프.cc
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#define MAX 
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int n;
+    cin>>n;
+    int arr[100'001];
+    for(int i=1;i<=n;i++){
+        cin>>arr[i];
+    }
+    int s;
+    cin>>s;
+
+    bool v[100'001]={0,};
+    queue<int> q;
+    q.push(s);
+    int cnt=1;
+    v[s]=1;
+
+    while(q.size()){
+        int c=q.front();
+        q.pop();
+        for(int nt:{c+arr[c],c-arr[c]}){
+            if(nt>=1&&nt<=n&&!v[nt]){
+                q.push(nt);
+                v[nt]=1;
+                cnt++;
+            }
+        }
+    }
+    
+    cout<<cnt;
+}
