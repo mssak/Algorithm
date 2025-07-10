3118b31c6c5891c0ae1e1dcce43d1e3ca1c93963
mssak <cxz017@naver.com>
2025-07-11 01:34:29 +0900

    [Silver I] Title: 경로 찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.cc
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -11,33 +11,27 @@ int main()
     int n;
     cin>>n;
 
-    int edge[MAX][MAX]={0,};
+    bool ans[MAX][MAX]={0,};
 
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            cin>>edge[i][j];
+            cin>>ans[i][j];
         }
     }
 
-    for(int s=0;s<n;s++){
-        queue<int> q;
-
-        int v[MAX]={0,};
-        q.push(s);
-
-        while(q.size()){
-            int c=q.front();
-            q.pop();
-
-            for(int i=0;i<n;i++){
-                if(edge[c][i] && !v[i]){
-                    v[i]=1;
-                    q.push(i);
+    for(int k=0;k<n;k++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(ans[i][k]&&ans[k][j]){
+                    ans[i][j]=1;
                 }
             }
         }
-        for(int i=0;i<n;i++){
-            cout<<v[i]<<" ";
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<ans[i][j]<<" ";
         }
         cout<<"\n";
     }
