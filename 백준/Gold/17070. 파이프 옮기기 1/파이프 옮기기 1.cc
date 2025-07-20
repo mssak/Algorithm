75f1befc30fed4c160d53ca13a7dc99ace14f3ac
mssak <cxz017@naver.com>
2025-07-21 03:03:09 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 232 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+#define MAX 
+using namespace std;
+using ll = long long;
+
+bool wall[32][32];
+int dp[32][32][3];
+int n;
+
+int f(int y,int x,int d){
+    if(y<0||y>=n||x<0||x>=n||wall[y][x]){
+        return 0;
+    }
+    if(d==1 && y!=0 && x!=0){
+        if(wall[y-1][x]||wall[y][x-1]){
+            return 0;
+        }
+    }
+
+    if(dp[y][x][d]!=-1){
+        return dp[y][x][d];
+    }
+
+    int ans;
+    if(d==0){
+        return f(y,x-1,0)+f(y,x-1,1);
+    }else if(d==1){
+        return f(y-1,x-1,0)+f(y-1,x-1,1)+f(y-1,x-1,2);
+    }else if(d==2){
+        return f(y-1,x,1)+f(y-1,x,2);
+    }
+
+    return dp[y][x][d]=ans;
+}
+
+int main()
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>wall[i][j];
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            dp[i][j][0]=-1;
+            dp[i][j][1]=-1;
+            dp[i][j][2]=-1;
+        }
+    }
+
+    dp[0][1][0]=1;
+
+    cout<<f(n-1,n-1,0)+f(n-1,n-1,1)+f(n-1,n-1,2)<<"\n";    
+}
