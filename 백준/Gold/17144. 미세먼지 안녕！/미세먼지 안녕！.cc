31fd9d05b2c6737eb34848a6dfc8f4fd890efc8f
mssak <cxz017@naver.com>
2025-07-29 06:21:05 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 36 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.cc
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.cc
@@ -9,90 +9,106 @@ int main()
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    int n,m,t;
-    cin>>n>>m>>t;
+    int n, m, t;
+    cin >> n >> m >> t;
 
-	vector<vector<int>> arr(n,vector<int>(m));
+    // 미리 2개의 격자(grid)를 생성
+    vector<vector<int>> grid1(n, vector<int>(m));
+    vector<vector<int>> grid2(n, vector<int>(m));
 
-	int cu=-1;
-	int cd;
-	for(int i=0;i<n;i++){
-		for(int j=0;j<m;j++){
-			cin>>arr[i][j];
-			if(arr[i][j]==-1){
-				if(cu==-1){
-					cu=i;
-				}else{
-					cd=i;
-				}
-			}
-		}
-	}
+    int cu = -1;
+    int cd;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            // 초기 상태는 grid1에 저장
+            cin >> grid1[i][j];
+            if(grid1[i][j] == -1){
+                if(cu == -1){
+                    cu = i;
+                } else {
+                    cd = i;
+                }
+            }
+        }
+    }
 
-	int dy[4]={0,-1,0,1};
-	int dx[4]={1,0,-1,0};
-	while(t--){
-		//spread
-		vector<vector<int>> tmp(n,vector<int>(m));
-		for(int i=0;i<n;i++){
-			for(int j=0;j<m;j++){
-				if(arr[i][j]>0){
-					int spreadCnt=0;
-					int tospread=arr[i][j]/5;
-					for(int d=0;d<4;d++){
-						int ny=i+dy[d];
-						int nx=j+dx[d];
-						if(ny>=0&&ny<n&&nx>=0&&nx<m&&arr[ny][nx]!=-1){
-							tmp[ny][nx]+=tospread;
-							spreadCnt++;
-						}
-					}
-					tmp[i][j]+=arr[i][j]-spreadCnt*tospread;
-				}
-			}
-		}
-		tmp[cu][0]=-1;
-		tmp[cd][0]=-1;
-		arr=tmp;
+    // 포인터를 사용해 현재 상태 격자와 다음 상태 격자를 가리킴
+    vector<vector<int>>* current_grid = &grid1;
+    vector<vector<int>>* next_grid = &grid2;
 
-		//up clean
-		for(int y=cu-2;y>=0;y--){
-			arr[y+1][0]=arr[y][0];
-		}
-		for(int x=1;x<m;x++){
-			arr[0][x-1]=arr[0][x];
-		}
-		for(int y=1;y<=cu;y++){
-			arr[y-1][m-1]=arr[y][m-1];
-		}
-		for(int x=m-2;x>0;x--){
-			arr[cu][x+1]=arr[cu][x];
-		}
-		arr[cu][1]=0;
+    int dy[4] = {0, -1, 0, 1};
+    int dx[4] = {1, 0, -1, 0};
 
-		//down clean
-		for(int y=cd+2;y<n;y++){
-			arr[y-1][0]=arr[y][0];
-		}
-		for(int x=1;x<m;x++){
-			arr[n-1][x-1]=arr[n-1][x];
-		}
-		for(int y=n-2;y>=cd;y--){
-			arr[y+1][m-1]=arr[y][m-1];
-		}
-		for(int x=m-2;x>0;x--){
-			arr[cd][x+1]=arr[cd][x];
-		}
-		arr[cd][1]=0;
-	}
+    while(t--){
+        // 1. 다음 상태를 계산하기 전에 next_grid를 0으로 초기화
+        for(int i = 0; i < n; ++i) {
+            fill((*next_grid)[i].begin(), (*next_grid)[i].end(), 0);
+        }
 
-	int ans=0;
-	for(int i=0;i<n;i++){
-		for(int j=0;j<m;j++){
-			if(arr[i][j]!=-1){
-				ans+=arr[i][j];
-			}
-		}
-	}
-	cout<<ans;
+        // 2. 확산 (current_grid를 읽어서 next_grid에 결과를 씀)
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if((*current_grid)[i][j] > 0){
+                    int spreadCnt = 0;
+                    int tospread = (*current_grid)[i][j] / 5;
+                    for(int d = 0; d < 4; d++){
+                        int ny = i + dy[d];
+                        int nx = j + dx[d];
+                        if(ny >= 0 && ny < n && nx >= 0 && nx < m && (*current_grid)[ny][nx] != -1){
+                            (*next_grid)[ny][nx] += tospread;
+                            spreadCnt++;
+                        }
+                    }
+                    (*next_grid)[i][j] += (*current_grid)[i][j] - spreadCnt * tospread;
+                }
+            }
+        }
+        (*next_grid)[cu][0] = -1;
+        (*next_grid)[cd][0] = -1;
+
+        // 3. 공기청정기 작동 (확산이 완료된 next_grid에 직접 적용)
+        // up clean
+        for(int y = cu - 2; y >= 0; y--){
+            (*next_grid)[y + 1][0] = (*next_grid)[y][0];
+        }
+        for(int x = 1; x < m; x++){
+            (*next_grid)[0][x - 1] = (*next_grid)[0][x];
+        }
+        for(int y = 1; y <= cu; y++){
+            (*next_grid)[y - 1][m - 1] = (*next_grid)[y][m - 1];
+        }
+        for(int x = m - 2; x > 0; x--){
+            (*next_grid)[cu][x + 1] = (*next_grid)[cu][x];
+        }
+        (*next_grid)[cu][1] = 0;
+
+        // down clean
+        for(int y = cd + 2; y < n; y++){
+            (*next_grid)[y - 1][0] = (*next_grid)[y][0];
+        }
+        for(int x = 1; x < m; x++){
+            (*next_grid)[n - 1][x - 1] = (*next_grid)[n - 1][x];
+        }
+        for(int y = n - 2; y >= cd; y--){
+            (*next_grid)[y + 1][m - 1] = (*next_grid)[y][m - 1];
+        }
+        for(int x = m - 2; x > 0; x--){
+            (*next_grid)[cd][x + 1] = (*next_grid)[cd][x];
+        }
+        (*next_grid)[cd][1] = 0;
+
+        // 4. 포인터 교체. 다음 루프에서는 역할이 바뀜
+        swap(current_grid, next_grid);
+    }
+
+    int ans = 0;
+    // 최종 결과는 current_grid에 저장되어 있음
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            if((*current_grid)[i][j] != -1){
+                ans += (*current_grid)[i][j];
+            }
+        }
+    }
+    cout << ans;
 }
