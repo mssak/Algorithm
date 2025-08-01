f238d134f8475f5eef43be07cc15280b4d53d210
mssak <cxz017@naver.com>
2025-08-02 02:58:13 +0900

    [Silver IV] Title: 30, Time: 4 ms, Memory: 2268 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.cc
+++ 백준/Silver/10610. 30/30.cc
@@ -1,2 +1,2 @@
 #include<bits/stdc++.h>
-using namespace std;main(){string s;cin>>s;sort(s.begin(),s.end(),greater<>());cout<<(s.back()=='0'&&accumulate(s.begin(),s.end(),0)%3==0?s:"-1");}
+using namespace std;main(){string s;cin>>s;sort(s.rbegin(),s.rend());cout<<(s.back()==48&&accumulate(s.begin(),s.end(),0)%3==0?s:"-1");}
